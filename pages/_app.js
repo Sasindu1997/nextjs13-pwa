155e7345a93cc673ea4f81b53acdec636f8884d0
Sasindu1997 <sasinduusgalhewa@gmail.com>
2023-09-14 09:54:12 +0530

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,24 @@
-import '@/styles/globals.css'
+// pages/_app.js
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { useEffect } from 'react';
+
+function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', () => {
+        navigator.serviceWorker
+          .register('/service-worker.js')
+          .then((registration) => {
+            console.log('Service Worker registered with scope:', registration.scope);
+          })
+          .catch((error) => {
+            console.error('Service Worker registration failed:', error);
+          });
+      });
+    }
+  }, []);
+
+  return <Component {...pageProps} />;
 }
+
+export default MyApp;
---
+++ public/service-worker.js
@@ -0,0 +1,9 @@
+// public/service-worker.js
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      })
+    );
+  });
